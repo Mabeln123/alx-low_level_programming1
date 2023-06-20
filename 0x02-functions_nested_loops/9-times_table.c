d1d5014de1f95edd6da3e2a4622355e4252de218
Mabeln123 <mabelnwoga@gmail.com>
2023-06-20 11:45:30 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,16 +17,16 @@ void times_table(void)
 
 			if ((n / 10) == 0)
 			{
-				if (j != 0)
-					_putchar(' ');
-				_putchar(n + '0');
+				if (j == 0)
+
+				_putchar('0');
 
 				if (j == 9)
 					continue;
 				_putchar(',');
 				_putchar(n + '0');
 
-				if (j == 9)
+				if (j < 9)
 					continue;
 				_putchar(',');
 				_putchar(' ');
@@ -35,7 +35,7 @@ void times_table(void)
 			{
 				_putchar((n / 10) + '0');
 				_putchar((n % 10) + '0');
-				if (j == 9)
+				if (j < 9)
 					continue;
 				_putchar(',');
 				_putchar(' ');
