298a1bdcb23e357c7723d8d9cfe79ebdde53e5ae
Mabeln123 <mabelnwoga@gmail.com>
2023-06-22 12:15:50 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * mu1 - multiplies two integers a and b
+ * @a: - The first integer
+ * @b: - The second integer
+ * Return: Always 0
+ */
+
+int mu1(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
